406499a977f963ce95673e1cece699ca26371a5c
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-12-03 18:06:13 +0900

    update day01

--- day01/Program.fs
+++ day01/Program.fs
@@ -1,54 +1,65 @@
 open Xunit
 open FsUnit.Xunit
 
-let tryFindDigit (text: string) (digitMap: Map<string, int>) =
-    digitMap
-    |> Map.tryPick (fun k v -> if text.StartsWith(k) then Some(v) else None)
+// https://learn.microsoft.com/ja-jp/dotnet/fsharp/language-reference/active-patterns#partial-active-patterns
+let (|StartsWith|_|) (p: string) (s: string) =
+    if s.StartsWith(p) then Some(s[p.Length ..]) else None
 
-let digits (text: string) digitMap =
-    // text の suffix をぜんぶ見る
-    [ 0 .. (text.Length - 1) ] |> Seq.map (fun i -> tryFindDigit text[i..] digitMap)
+let parseNoramlDigit (text: string) =
+    match text with
+    | StartsWith "1" _rest -> Some(1)
+    | StartsWith "2" _rest -> Some(2)
+    | StartsWith "3" _rest -> Some(3)
+    | StartsWith "4" _rest -> Some(4)
+    | StartsWith "5" _rest -> Some(5)
+    | StartsWith "6" _rest -> Some(6)
+    | StartsWith "7" _rest -> Some(7)
+    | StartsWith "8" _rest -> Some(8)
+    | StartsWith "9" _rest -> Some(9)
+    | _ -> None
 
-let firstDigit text digitMap =
-    (text, digitMap) ||> digits |> Seq.pick id
+let parseLettersDigit (text: string) =
+    match text with
+    | StartsWith "one" _rest -> Some(1)
+    | StartsWith "two" _rest -> Some(2)
+    | StartsWith "three" _rest -> Some(3)
+    | StartsWith "four" _rest -> Some(4)
+    | StartsWith "five" _rest -> Some(5)
+    | StartsWith "six" _rest -> Some(6)
+    | StartsWith "seven" _rest -> Some(7)
+    | StartsWith "eight" _rest -> Some(8)
+    | StartsWith "nine" _rest -> Some(9)
+    | _ -> None
 
-let lastDigit text digitMap =
-    (text, digitMap) ||> digits |> Seq.rev |> Seq.pick id
+let collectDigits (text: string) (parseDigit: string -> int option) =
+    let rec collect t acc =
+        if String.length t = 0 then
+            Seq.rev acc
+        else
+            match parseDigit t with
+            | Some((v)) -> collect t[1..] (v :: acc)
+            | None -> collect t[1..] acc
 
-let solve texts digitMap =
+    collect text []
+
+let solve texts parseDigit =
     texts
     |> Seq.sumBy (fun t ->
-        let first = firstDigit t digitMap
-        let last = lastDigit t digitMap
+        let digits = collectDigits t parseDigit
+        let first = Seq.head digits
+        let last = Seq.last digits
         first * 10 + last)
 
-
-let normalDigit =
-    [ ("1", 1)
-      ("2", 2)
-      ("3", 3)
-      ("4", 4)
-      ("5", 5)
-      ("6", 6)
-      ("7", 7)
-      ("8", 8)
-      ("9", 9) ]
-
-let lettersDigit =
-    [ ("one", 1)
-      ("two", 2)
-      ("three", 3)
-      ("four", 4)
-      ("five", 5)
-      ("six", 6)
-      ("seven", 7)
-      ("eight", 8)
-      ("nine", 9) ]
-
-let part1 (texts: string seq) = solve texts (Map normalDigit)
+let part1 (texts: string seq) = solve texts parseNoramlDigit
 
 let part2 (texts: string seq) =
-    solve texts (Map(normalDigit @ lettersDigit))
+    let parseDigit t =
+        parseNoramlDigit t
+        |> Option.orElseWith (fun () ->
+            // fallback
+            parseLettersDigit t)
+
+    solve texts parseDigit
 
 let parse (input: string) = input.Split("\n")
 
@@ -62,6 +73,11 @@ a1b2c3d4e5f
 treb7uchet"
 
         parse input |> part1 |> should equal 142
+
+    [<Fact>]
+    let testCollectDigits () =
+        // こういう入力は two/ne ではなく two, one と扱う
+        collectDigits "twone" parseLettersDigit |> should equalSeq (seq [ 2; 1 ])
 
     [<Fact>]
     let testPart2 () =
